44b11d87b9bcac81fd4e6dc1e7ae2fe987327224
kiligiris <79125157+kiligiris@users.noreply.github.com>
2021-10-07 14:41:31 +0900

    バグの修正とアップデート
    複数ファイルの表示に対応できていなかったので修正しました
    先頭にファイル名を表示するようにしました
    テスト用にabc.txtファイルを追加しました

--- cat.py
+++ cat.py
@@ -2,21 +2,25 @@
 # coding: utf-8
 
 import sys
-import fileinput
 from pathlib import Path
 
 def cat(fn):
-    if Path(fn).exists():
-        for n, line in enumerate(fileinput.input()):
-            line = line.replace('\n', '')
-            print('{0}：{1}'.format(n, line))
+    if Path(fn).exists(): #ファイルが存在するなら
+        f = open(fn,"r")
+        for n, line in enumerate(f):
+            line = line.replace('\n', '') #余計な改行の削除
+            print('{0}：{1}'.format(n, line)) #行数とテキストの表示
+        f.close()
     else:
         print("ファイル名が間違っています")
         
-args = sys.argv
+args = sys.argv #コマンドライン引数の受け取り
 
-if 2 <= len(args):
+if 2 <= len(args): #引数が入力されていたら
     for i in range(1,len(args)):
-        cat(args[i])
+        print(args[i]) #ファイル名表示
+        cat(args[i])   #ファイルの内容を表示
+        if i + 1 < len(args): #最後のファイルでなければ
+            print()           #改行を挿入
 else:
     print('ファイル名を入力してください')
